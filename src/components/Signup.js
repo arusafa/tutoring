4dfad2ede11650d58c510fbff883a316b5c56075
arusafa <arusafacollege@gmail.com>
2023-02-06 02:58:48 -0500

    updated to have database

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,64 +1,91 @@
-import React, { useRef, useState } from "react";
-import { Form, Card, Button, Alert } from "react-bootstrap";
-import { useAuth } from "../context/AuthContext";
+import React, { useState, useEffect } from "react";
+import firebase from "firebase/compat/app";
+import "firebase/auth";
+import "firebase/firestore";
 import { Link } from "react-router-dom";
-import "firebase/compat/firestore";
 
-export default function Signup() {
-  const emailRef = useRef();
-  const passwordRef = useRef();
-  const passwordConfirmRef = useRef();
-  const { signup, currentUser } = useAuth();
-  const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
+const SignUp = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [currentUser, setCurrentUser] = useState(null);
 
-  async function handleSubmit(e) {
-    e.preventDefault();
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setCurrentUser(user);
+      } else {
+        setCurrentUser(null);
+      }
+    });
+    return () => unsubscribe();
+  }, []);
 
-    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-      return setError("Passwords do not match");
+  const handleSignUp = () => {
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+      return;
     }
-    try {
-      setError("");
-      setLoading(true);
-      await signup(emailRef.current.value, passwordRef.current.value);
-    } catch {
-      setError("Failed to create an account");
-    }
-    setLoading(false);
-  }
+
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((user) => {
+        firebase
+          .firestore()
+          .collection("tutors")
+          .doc(user.user.uid)
+          .set({ username, email, phone });
+        alert("You have been signed up");
+        console.log(user);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
 
   return (
-    <>
-      <Card>
-        <Card.Body>
-          <h2 className="text- mb-0">Student</h2>
-          <h2 className="text-center mb-4">Sign Up</h2>
-          {currentUser && currentUser.email}
-          {error && <Alert variant="danger">{error}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
-            </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" ref={passwordRef} required />
-            </Form.Group>
-            <Form.Group id="password-confirm">
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type="password" ref={passwordConfirmRef} required />
-            </Form.Group>
-            <br />
-            <Button disabled={loading} className="w-100" type="submit">
-              Sign Up
-            </Button>
-          </Form>
-        </Card.Body>
-      </Card>
+    <div>
+      <h1>Sign Up</h1>
+      {currentUser ? <h1>Signed User / {currentUser.email}</h1> : null}
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Confirm Password"
+        value={confirmPassword}
+        onChange={(e) => setConfirmPassword(e.target.value)}
+      />
+      <input
+        type="tel"
+        placeholder="Phone"
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+      />
+      <button onClick={handleSignUp}>Sign Up</button>
       <div className="w-100 text-center mt-2">
         Already have an account? <Link to="/login">Log In</Link>
       </div>
-    </>
+    </div>
   );
-}
+};
+
+export default SignUp;
